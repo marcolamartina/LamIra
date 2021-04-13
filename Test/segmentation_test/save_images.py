f5d001458b232e909d37b9df638e2c8d41cf9187
Davide Iraci <davide.iraci@community.unipa.it>
2021-04-13 20:03:25 +0200

    Final segmentation test

--- Test/segmentation_test/save_images.py
+++ Test/segmentation_test/save_images.py
@@ -3,11 +3,7 @@ import freenect
 import cv2
 import os
 import numpy as np
-
-cv2.namedWindow('Depth')
-cv2.namedWindow('Video')
-print('Press ESC in window to stop')
-
+import time
 
 def pretty_depth(depth):
     """Converts depth into a 'nicer' format for display
@@ -74,17 +70,17 @@ def get_contours(grey):
 
 
 def get_mask(depth,start=1,slices_selected=4,slices=5):
-        minimum=np.min(depth[depth>0])
-        depth_masked=depth.copy()
-        depth_masked[depth==255]=0
-        maximum=np.max(depth_masked)
-        step=(maximum-minimum)/slices
-        threshold_1=(step*(start-1))+minimum
-        threshold_2=threshold_1+(step*slices_selected)
-        depth_masked[depth < threshold_1] = 0
-        #depth_masked[depth >= threshold_1] = 255
-        depth_masked[depth > threshold_2] = 0
-        return depth_masked
+    minimum=np.min(depth[depth>0])
+    depth_masked=depth.copy()
+    depth_masked[depth==255]=0
+    maximum=np.max(depth_masked)
+    step=(maximum-minimum)/slices
+    threshold_1=(step*(start-1))+minimum
+    threshold_2=threshold_1+(step*slices_selected)
+    depth_masked[depth < threshold_1] = 0
+    #depth_masked[depth >= threshold_1] = 255
+    depth_masked[depth > threshold_2] = 0
+    return depth_masked
 
 def crop_image(image, zoom):
     image_h, image_w = image.shape
@@ -99,11 +95,107 @@ def crop_image(image, zoom):
     delta_w=int(w_desired-image_w/2)
     return  image[delta_h:image_h+delta_h, delta_w:image_w+delta_w]
 
+def padding(image, border_size=10):
+    return cv2.copyMakeBorder(image, border_size,border_size,border_size,border_size,cv2.BORDER_CONSTANT,value=0)
 
-if __name__=="__main__":
+def crop(image, border_size=10):
+    return image[border_size:image.shape[0]-border_size, border_size:image.shape[1]-border_size]
+
+def get_point(image,border_size=10,tolerance=10):
+    return (image.shape[0]-border_size-tolerance,image.shape[1]-border_size-tolerance)
+
+def removing_bottom_floor(image):
+    # Clear rows with uniform values (First floor removal)
+    for i in range(image.shape[0]-1,-1, -1):
+        row = image[i, :]
+        if (np.max(row) - np.min(row)) < 8:
+            image[i, :]=0
+    return image
+
+def get_neighbours(point, max_dimensions):
+    l=[(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+    return {(point[0]+x,point[1]+y) for x,y in l if 0<=point[0]+x<max_dimensions[0] and 0<=point[1]+y<max_dimensions[1]}            
+
+def removing_floor_cython(image, _output, border_size=10, point=None, tolerance=15):
+    import pyximport
+    pyximport.install(setup_args={'include_dirs': np.get_include()})
+    from region_growing_cython import grow_region_cython
+
+    if not point:
+        point = get_point(image,border_size)
+
+    region_grow_cython = grow_region_cython(image, point, tolerance)
+    cv2.imshow("Region Grow Cython", region_grow_cython)    
+    
+def removing_floor_region_growing(image, output, border_size=10, point=None, tolerance=25):
+    if not point:
+        point = get_point(image,border_size)
+    result=output.copy()    
+    value_ref=int(image[point[0],point[1]] )   
+    neighbours={point}
+    visited=set()
+    while len(neighbours)>0:
+        p=neighbours.pop()
+        n=set()
+        n={x for x in get_neighbours(p, image.shape) if x not in visited and abs(int(image[x[0],x[1]])-value_ref)<tolerance}    
+        neighbours.update(n)
+        result[p[0],p[1]]=0
+        visited.add(p)   
+    return result
+
+def removing_floor_connected_components(original, output, border_size=10, point=None, tolerance=25):
+    if not point:
+        point = get_point(original,border_size)
+    image=original.copy()
+    result=output.copy()    
+    value_ref=int(image[point[0],point[1]] )    
+    image[image<value_ref-tolerance]=0
+    image[image>value_ref+tolerance]=0
+    image[image>0]=1
+    _, labels = cv2.connectedComponents(image)
+    label=labels[point[0],point[1]]
+    result[labels==label]=0
+    return result
+
+
+
+def live_mode():
+    while True:
+        # Original depth
+        dep_original=get_depth()
+ 
+        # Remove background
+        depth_no_background=get_mask(dep_original)
+
+        # Filtering depth
+        depth_no_background_median = cv2.medianBlur(depth_no_background, 5)
+
+        # Cropping image
+        border_size=10
+        depth_no_background_median_cropped = crop(depth_no_background_median,border_size=border_size)
+        # Padding image for restore dimensions        
+        depth_no_background_median_cropped = padding(depth_no_background_median_cropped,border_size=border_size)
+
+        # Sobel
+        depth_sobel = cv2.Sobel(dep_original,cv2.CV_64F,0,1,ksize=5)
+        abs_sobel64f = np.absolute(depth_sobel)
+        depth_sobel = np.uint8(abs_sobel64f)
+
+        # Blurring Sobel image
+        kernel_size=9
+        depth_sobel_blurred = cv2.blur(depth_sobel, (kernel_size,kernel_size))
+
+        depth_without_floor=removing_floor_connected_components(depth_sobel_blurred,depth_no_background_median_cropped,border_size)
+        depth_without_floor_blurred=cv2.medianBlur(depth_without_floor, 15)
+        cv2.imshow('Depth Without Backgroud and Floor', depth_without_floor_blurred)
+        cv2.imshow('Depth Sobel', depth_sobel_blurred)
+
+        if cv2.waitKey(1)==27:
+            break
+
+def batch_mode():
     from_kinect=False
-
-
+    
     for i in range(1):
         # Defining starting element
         path=os.path.dirname(__file__)
@@ -112,19 +204,28 @@ if __name__=="__main__":
 
         # Take depth image
         if from_kinect:
+            cv2.namedWindow('Depth')
+            cv2.namedWindow('Video')
             dep_original=get_depth()
-        else:
-            dep_original=cv2.imread(depth_file+"_starting.png")
             v=get_video()
+        else:
+            dep_original=cv2.imread(depth_file+"_starting.png", 0)
+            v=None
 
+        
         # Remove background by mask
-        depth_masked=get_mask(dep_original)
+        depth_masked=get_mask(dep_original)       
 
         # Filtering depth
-
         d_median = cv2.medianBlur(depth_masked, 5)
-        bord = 10
-        d_median_cropped = d_median[bord:d_median.shape[0]-bord, bord:d_median.shape[1]-bord]
+
+        border_size=10
+        # Cropping image
+        d_median_cropped = crop(d_median,border_size=border_size)
+        # Padding image for restore dimensions        
+        d_median_cropped = padding(d_median_cropped,border_size=border_size)
+        
+        
 
         #d_edge = cv2.Canny(depth_masked,55,65,apertureSize = 5)
         #d_edge_2 = cv2.morphologyEx(d_edge, cv2.MORPH_GRADIENT, element)
@@ -137,15 +238,27 @@ if __name__=="__main__":
             v_edge=cv2.Canny(v_grey,70,90, apertureSize = 3)
             rgb_file=path+"/test/rgb_"+str(i)
 
-        # Clear rows with uniform values (First floor removal)
-        for i in range(d_median_cropped.shape[0]-1,-1, -1):
-            row = d_median_cropped[i, :]
-            if (np.max(row) - np.min(row)) < 8:
-                d_median_cropped[i, :]=0
 
         # Save interediet file
         d_first_attempt = d_median_cropped.copy()
         d_final2 = get_mask(d_first_attempt, slices_selected=15, slices=16)
+
+        #cv2.imwrite(depth_file+"_starting.png", dep_original)
+
+        depth_sobel = cv2.Sobel(dep_original,cv2.CV_64F,0,1,ksize=5)
+        abs_sobel64f = np.absolute(depth_sobel)
+        depth_sobel = np.uint8(abs_sobel64f)
+
+        kernel_size=9
+        depth_test = cv2.blur(depth_sobel, (kernel_size,kernel_size))
+        cv2.imshow("sobel", depth_sobel)
+        cv2.imshow("pippo", depth_test)
+
+        image_without_floor=removing_floor_region_growing(depth_test,d_median_cropped,border_size)
+        image_without_floor=cv2.medianBlur(image_without_floor, 15)  
+        cv2.imshow("not floor", image_without_floor)
+        cv2.waitKey(0)
+
 
         # Clear clomuns with floor
         for i in range(d_median_cropped.shape[1]-1,-1, -1):
@@ -190,3 +303,5 @@ if __name__=="__main__":
         cv2.imwrite(rgb_file+".png", v)
         cv2.imwrite(rgb_file+"_edge.png", v_edge)
         '''
+if __name__=="__main__":
+    live_mode()         
