0e6c3a36ae3ed0772ac5eb51eb1f29a382f83c0f
Davide Iraci <davide.iraci@community.unipa.it>
2021-04-15 15:30:23 +0200

    Test K D Tree

--- Test/kd_tree_test/kd_tree_test.py
+++ Test/kd_tree_test/kd_tree_test.py
@@ -4,11 +4,21 @@ from sklearn.neighbors import KDTree
 
 
 rng = np.random.RandomState(0)
-X = rng.random_sample((10, 3))  # 10 points in 3 dimensions
-X=[random.randint(10) for _ in range(10)]
-tree = KDTree(X, leaf_size=2)        
-                
-dist, ind = tree.query(X[:1], k=3)     
-print(ind)  # indices of 3 closest neighbors
+X = rng.random_sample((1000, 10))  # 10 points in 3 dimensions
+tree = KDTree(X,metric='euclidean')        
+#print(X)
+p=rng.random_sample((1, 10))
+point=np.array(p[0])                
+dist, ind = tree.query(p, k=100)
+f=tree.get_arrays()
+real_distances=[]
+distances=[]
 
-print(dist)  # distances to 3 closest neighbors
+# Get points from index
+#print(np.asarray(tree.data[index]))
+
+
+for distance,index in zip(dist[0],ind[0]): 
+    print("indice {} punto {}".format(index,f[0][index]))  # indices of 3 closest neighbors
+    print("distanza: {}".format(distance))  # distances to 3 closest neighbors
+
