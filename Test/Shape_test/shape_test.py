316bc80607b52d5f642237dc0c98b1c88716a832
Davide Iraci <davide.iraci@community.unipa.it>
2021-04-16 16:30:03 +0200

    Dataset upload
    Camera

--- Test/Shape_test/shape_test.py
+++ Test/Shape_test/shape_test.py
@@ -102,14 +102,13 @@ def calculate_symmetry_2d(mask):
             mask=mask.T
         half=int(mask.shape[1]/2)
         first_half = mask[:, 0:half]
-        second_half = mask[:, half+(mask.shape[0] % 2):]
-        cv2.imshow("first",first_half)
-        cv2.imshow("second",second_half)
-        cv2.waitKey(0)
+        second_half = mask[:, half+(mask.shape[1] % 2):]
         second_half = np.flip(second_half, axis=0)
-        symmetry = np.sum(first_half == second_half).all()
-        symmetries.append(symmetry)        
-    return max(symmetries)/cv2.countNonZero(mask)
+        symmetry = np.sum(first_half == second_half)
+        symmetries.append(symmetry)
+        
+    print(symmetries,cv2.countNonZero(mask))               
+    return float(max(symmetries)/(cv2.countNonZero(mask)))
 
 def euclidean_distance(a,b):
     return numpy.linalg.norm(a-b)
