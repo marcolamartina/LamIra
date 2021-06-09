9061415ee9c995b13dd3f8db1cbad9da025d0056
Marco La Martina <marco.lamartina@community.unipa.it>
2021-06-09 11:04:39 +0200

    benchmarks

--- Benchmark/KDTree/KDTree_benchmark.py
+++ Benchmark/KDTree/KDTree_benchmark.py
@@ -1,12 +1,12 @@
 import numpy as np
+from sklearn.neighbors import KDTree
 import matplotlib.pyplot as plt
-from scipy.spatial import ConvexHull
 import random
-import time
+import time 
 
 rng = np.random.RandomState(0)
-possible_dimensions=range(2,14,1)
-possible_dimensions_sample=range(20,201,20)
+possible_dimensions=range(5,100,5)
+possible_dimensions_sample=range(200,10001,200)
 
 dimension=9
 x_sample=[]
@@ -14,33 +14,33 @@ y_sample=[]
 for dimension_sample in possible_dimensions_sample:
     pts = rng.random_sample((dimension_sample, dimension))
     starting=time.time()
-    hull=ConvexHull(pts)
+    tree = KDTree(pts)
     time_elapsed=time.time()-starting
     x_sample.append(dimension_sample)
-    y_sample.append(time_elapsed)
+    y_sample.append(time_elapsed*1000)
     print(time_elapsed)
 
-dimension_sample=60
+dimension_sample=1000
 x_dim=[]
 y_dim=[]
 for dimension in possible_dimensions:
     pts = rng.random_sample((dimension_sample, dimension))
     starting=time.time()
-    hull=ConvexHull(pts)
+    tree = KDTree(pts)
     time_elapsed=time.time()-starting
     x_dim.append(dimension)
-    y_dim.append(time_elapsed)
+    y_dim.append(time_elapsed*1000)
     print(time_elapsed)
 
 plt.subplot(2, 1, 1)
-plt.title("Convex Hull Benchmark")
+plt.title("KDTree Benchmark")
 plt.xlabel("Sample Dimension")
-plt.ylabel("Time(s)")
+plt.ylabel("Time(ms)")
 plt.plot(x_sample,y_sample)
 
 plt.subplot(2, 1, 2)
 plt.xlabel("Dimensionality")
-plt.ylabel("Time(s)")
+plt.ylabel("Time(ms)")
 plt.plot(x_dim,y_dim)
 
 plt.show()    
--- Benchmark/SVM_linear/SVM_benchmark.py
+++ Benchmark/SVM_linear/SVM_benchmark.py
@@ -1,12 +1,14 @@
 import numpy as np
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import SGDClassifier 
 import matplotlib.pyplot as plt
-from scipy.spatial import ConvexHull
 import random
-import time
+import time 
 
 rng = np.random.RandomState(0)
-possible_dimensions=range(2,14,1)
-possible_dimensions_sample=range(20,201,20)
+possible_dimensions=range(5,100,5)
+possible_dimensions_sample=range(200,10001,200)
 
 dimension=9
 x_sample=[]
@@ -14,33 +16,37 @@ y_sample=[]
 for dimension_sample in possible_dimensions_sample:
     pts = rng.random_sample((dimension_sample, dimension))
     starting=time.time()
-    hull=ConvexHull(pts)
+    labels=np.array([random.randint(0,10) for _ in range(dimension_sample)])
+    clf = make_pipeline(StandardScaler(), SGDClassifier()) 
+    clf.fit(pts,labels)
     time_elapsed=time.time()-starting
     x_sample.append(dimension_sample)
-    y_sample.append(time_elapsed)
+    y_sample.append(time_elapsed*1000)
     print(time_elapsed)
 
-dimension_sample=60
+dimension_sample=1000
 x_dim=[]
 y_dim=[]
 for dimension in possible_dimensions:
     pts = rng.random_sample((dimension_sample, dimension))
     starting=time.time()
-    hull=ConvexHull(pts)
+    labels=np.array([random.randint(0,10) for _ in range(dimension_sample)])
+    clf = make_pipeline(StandardScaler(), SGDClassifier()) 
+    clf.fit(pts,labels)
     time_elapsed=time.time()-starting
     x_dim.append(dimension)
-    y_dim.append(time_elapsed)
+    y_dim.append(time_elapsed*1000)
     print(time_elapsed)
 
 plt.subplot(2, 1, 1)
-plt.title("Convex Hull Benchmark")
+plt.title("Linear SVM Benchmark")
 plt.xlabel("Sample Dimension")
-plt.ylabel("Time(s)")
+plt.ylabel("Time(ms)")
 plt.plot(x_sample,y_sample)
 
 plt.subplot(2, 1, 2)
 plt.xlabel("Dimensionality")
-plt.ylabel("Time(s)")
+plt.ylabel("Time(ms)")
 plt.plot(x_dim,y_dim)
 
 plt.show()    
