34dbee4a0dd2f2698ac1dd4b412eec971a80c67d
Marco La Martina <marco.lamartina@community.unipa.it>
2021-06-13 15:34:20 +0200

    plotting knowledge example

--- src/Grounding/plotting.py
+++ src/Grounding/plotting.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import random
 
-def show_space(X,y,legend=False):
+def show_spaces(X,y,legend=False):
     fig = plt.figure(num="Knowledge Space",figsize=(8,8))
     color={}
     for space_index,space_label in enumerate(X.keys()):
@@ -27,6 +27,30 @@ def show_space(X,y,legend=False):
         for i,label in axis_label.items():
             eval("ax.set_{:s}label('{:s}')".format(i, label))  
 
+
+def show_space(X,y,space_label="general",legend=False):
+    fig = plt.figure(num="Knowledge Space",figsize=(8,8))
+    X_current=X[space_label]
+    y_current=y[space_label]
+    test_set=["aglio","cipolla","spazzolino"]
+    color={}
+    ax = fig.add_subplot(1,1,1, projection="3d")
+    ax.set_title(space_label)
+    for point,label in zip(X_current,y_current):
+        if label not in test_set or (point[0]<-10 and point[1]>-2):
+            continue
+        if label not in color.keys():
+            color[label]=[random.random() for _ in range(3)]
+            #ax.text(point[0],point[1],point[2], label, size=10, zorder=1, color='k')
+            if legend:
+                ax.plot(point[0],point[1],point[2], "o", color=color[label],label=label) 
+        else:
+            ax.plot(point[0],point[1],point[2], "o", color=color[label])
+    if legend:
+        ax.legend(loc='upper left')        
+    axis_label={"x":"x", "y":"y", "z":"z"}    
+    for i,label in axis_label.items():
+        eval("ax.set_{:s}label('{:s}')".format(i, label))
 
 data_dir_grounding = os.path.dirname(__file__)
 data_dir_base_knowledge = os.path.join(data_dir_grounding,"base_knowledge")
@@ -56,7 +80,7 @@ for space_label,X in data["X"].items():
 '''    
     
 # Plotting
-show_space(data["X_transformed"],data["y"])
+show_space(data["X_transformed"],data["y"],legend=True)
 plt.show()
 
  
