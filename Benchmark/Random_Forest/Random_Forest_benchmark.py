03a1197128030c9a5e388beb7755b8fde22a8b08
Marco La Martina <marco.lamartina@community.unipa.it>
2021-06-23 11:06:19 +0200

    benchmark

---
+++ Benchmark/Random_Forest/Random_Forest_benchmark.py
@@ -0,0 +1,50 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import random
+import time 
+
+rng = np.random.RandomState(0)
+possible_dimensions=range(5,100,5)
+possible_dimensions_sample=range(200,10001,200)
+
+dimension=9
+x_sample=[]
+y_sample=[]
+for dimension_sample in possible_dimensions_sample:
+    pts = rng.random_sample((dimension_sample, dimension))
+    starting=time.time()
+    labels=np.array([random.randint(0,10) for _ in range(dimension_sample)])
+    clf = RandomForestClassifier(n_jobs=-1, n_estimators=30)
+    clf.fit(pts,labels)
+    time_elapsed=time.time()-starting
+    x_sample.append(dimension_sample)
+    y_sample.append(time_elapsed)
+    print(time_elapsed)
+
+dimension_sample=1000
+x_dim=[]
+y_dim=[]
+for dimension in possible_dimensions:
+    pts = rng.random_sample((dimension_sample, dimension))
+    starting=time.time()
+    labels=np.array([random.randint(0,10) for _ in range(dimension_sample)])
+    clf = RandomForestClassifier(n_jobs=-1, n_estimators=30)
+    clf.fit(pts,labels)
+    time_elapsed=time.time()-starting
+    x_dim.append(dimension)
+    y_dim.append(time_elapsed)
+    print(time_elapsed)
+
+plt.subplot(2, 1, 1)
+plt.title("Random Forest Benchmark")
+plt.xlabel("Sample Dimension")
+plt.ylabel("Time(s)")
+plt.plot(x_sample,y_sample)
+
+plt.subplot(2, 1, 2)
+plt.xlabel("Dimensionality")
+plt.ylabel("Time(s)")
+plt.plot(x_dim,y_dim)
+
+plt.show()    
